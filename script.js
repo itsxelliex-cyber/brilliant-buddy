e249fb91904dc95c3e263e8e2199744bb94f62db
Elroi <itsxelliex@gmail.com>
2025-09-12 21:06:20 -0400

    forgot to commit! i was so focused on fixing the bugs with the javascript and the greetings, however they finally work! plenty of changes

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 
-window.onload = function() {
-    const gallery = document.querySelector('.gallery-container');
-    let scrollAmount = 0;
-    const imageHeight = 250; // match your .gallery-image height
-    const totalImages = gallery.querySelectorAll('.gallery-image').length;
-    const maxScroll = imageHeight * (totalImages - 1);
-    let direction = 1; // 1 for down, -1 for up
+document.getElementById("nextButton").addEventListener("click", function() {
+  const nameInput = document.getElementById("nameInput");
+   const name = nameInput.value.trim();
+   if (!name) {
+      alert("Please enter your name.");
+      nameInput.focus();
+      return;
+   }
+  let target = document.getElementById("roleSelect").value === "student"
+    ? "studentportal.html"
+    : "teacherportal.html";
+  window.location.href = `${target}?name=${name}`;
+});
+  
 
-    function scrollGallery() {
-        scrollAmount += 3 * direction; // pixels per frame, adjust for speed
-        if (scrollAmount > maxScroll) {
-            direction = -1; // loop back to start
-        }
-        if (scrollAmount < 0) {
-          direction = 1;
-        }
-        gallery.style.transform = `translateY(-${scrollAmount}px)`;
-        requestAnimationFrame(scrollGallery);
-    }
-
-    scrollGallery();
-};
